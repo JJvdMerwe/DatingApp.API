81046546aa8d20922b8345a1243641e10ddb18c2
jordaan <jorvdmerwe@gmail.com>
2019-10-01 23:24:10 +0200

    Configure DbContext service for SqlServer

--- Startup.cs
+++ Startup.cs
@@ -35,7 +35,8 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext>((options 
+                => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2).AddJsonOptions(opt =>{
                 opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             });
